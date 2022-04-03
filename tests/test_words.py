ba9e69e2bbd3088cd060923612589e8429dac0bf
Chandan Chainani <chainanichan@gmail.com>
2022-04-03 21:06:03 +0530

    Added create word test

--- tests/test_words.py
+++ tests/test_words.py
@@ -1,3 +1,9 @@
 def test_get_words(client):
     response = client.get("/words")
     assert response.status_code == 200
+
+def test_create_word(client):
+    response = client.post("/words", json={
+        "word": "Flask"
+    })
+    assert response.status_code == 201
