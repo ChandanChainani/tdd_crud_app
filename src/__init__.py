6b9b63d857ae15798602bf9b3caaf2af92c0df27
Chandan Chainani <chainanichan@gmail.com>
2022-04-03 20:12:38 +0530

    Added sqlite db setup

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,15 @@
+import os
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 
 @app.route("/words")
 def words():
     words = []
     return jsonify(words)
+
