d547b6bad919a948d027ba924f32b32e7707636d
Chandan Chainani <chainanichan@gmail.com>
2022-04-03 22:21:51 +0530

    Added update word test

--- tests/test_words.py
+++ tests/test_words.py
@@ -14,3 +14,10 @@ def test_delete_word(client):
         "word": "Flask"
     })
     assert response.status_code == 200
+
+def test_update_word(client):
+    response = client.put("/words", json={
+        "id": 3,
+        "word": "Flask App"
+    })
+    assert response.status_code == 200
