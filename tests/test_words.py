ba651eeb2933a25b753b5c409f37c1d7616edc8e
Chandan Chainani <chainanichan@gmail.com>
2022-04-03 23:31:00 +0530

    added no parameter test cases

--- tests/test_words.py
+++ tests/test_words.py
@@ -2,6 +2,10 @@ def test_get_words(client):
     response = client.get("/words")
     assert response.status_code == 200
 
+def test_create_word_no_parameters(client):
+    response = client.post("/words", json={})
+    assert response.status_code == 400
+
 def test_create_word(client):
     response = client.post("/words", json={
         "word": "Flask"
@@ -15,12 +19,20 @@ def test_delete_word(client):
     })
     assert response.status_code == 200
 
+def test_delete_word_no_parameters(client):
+    response = client.post("/words", json={})
+    assert response.status_code == 400
+
 def test_update_word(client):
     response = client.put("/words", json={
         "id": 3,
         "word": "Flask App"
     })
     assert response.status_code == 200
+
+def test_update_word_no_parameters(client):
+    response = client.put("/words", json={})
+    assert response.status_code == 400
 
 def test_get_word_by_id(client):
     response = client.get("/words")
